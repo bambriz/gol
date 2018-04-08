30e9fa0ca43d3eee481b257a70604be1782f3da6
Bryan Ambriz <beambriz2017@gmail.com>
2018-04-08 15:32:54 -0700

    added more stuff

--- src/index.js
+++ src/index.js
@@ -70,6 +70,51 @@ class Main extends React.Component {
         });
     }
 
+    seed = () => {
+        
+        let gridCopy = arrayClone(this.state.gridFull);
+        for (let i = 0; i < this.rows; i++){
+            for(let j = 0; j< this.cols; j++){
+                if(Math.floor(Math.random()*4 )=== 1){
+                    gridCopy[i][j] = true;
+                }
+            }
+        }
+        this.setState({
+            gridFull: gridCopy
+        });
+    }
+
+
+    playButton = () => {
+        clearInterval(this.intervalId);
+        this.intervalId = setInterval(this.play, this.speed);
+    }
+
+    play = () => {
+        let g = this.state.gridFull;
+        let g2 = arrayClone(this.state.gridFull);
+
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+              let count = 0;
+              if (i > 0) if (g[i - 1][j]) count++;
+              if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+              if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+              if (j < this.cols - 1) if (g[i][j + 1]) count++;
+              if (j > 0) if (g[i][j - 1]) count++;
+              if (i < this.rows - 1) if (g[i + 1][j]) count++;
+              if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+              if (i < this.rows - 1 && this.cols - 1) if (g[i + 1][j + 1]) count++;
+              if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+              if (!g[i][j] && count === 3) g2[i][j] = true;
+            }
+        }
+    }
+
+    componentDidMount(){
+        this.seed();
+    }
     render(){
         return(
             <div>
